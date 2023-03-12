3d952bdf892dde2126a55b74ddafe867e46fa33d
satoshi komatsu <satoshi7190@gmail.com>
2023-03-12 16:10:24 +0900

    fix

--- draft.js
+++ draft.js
@@ -2,43 +2,17 @@
 Cesium.Ion.defaultAccessToken =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlZmUyZGRjNy01ODhiLTRlOGMtYmIxMC03YWYwNzIwZjRlOTYiLCJpZCI6OTI1MzgsImlhdCI6MTY3NzA3NTY1N30.otNcG_H9SIbgG-TqH5PXCfqRqiY-g30AeBDInIbzdJI';
 
-Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(134.355368, 35.4591744, 134.3774097, 35.4727003);
-Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
+// 地形データの読み込み;
+const terrainProvider = Cesium.createWorldTerrain();
 
-// Viewerを表示、地形の読み込み、不要なボタン等はオフに
+// ビューアーを作成
 const viewer = new Cesium.Viewer('cesiumContainer', {
     contextOptions: {
         requestWebgl: true,
     },
-    terrainProvider: Cesium.createWorldTerrain(),
-
-    // 不要なボタン等はオフに
-    timeline: false,
-    animation: false,
-    homeButton: false,
-    vrButton: false,
-    geocoder: false,
-    sceneModePicker: false,
-    navigationHelpButton: false,
-    shadows: false,
-    shouldAnimate: false,
-    fullscreenButton: false,
-    sceneModePicker: false,
-    scene3DOnly: true,
-    requestRenderMode: true,
-    // maximumRenderTimeChange: Infinity,
-    navigationInstructionsInitiallyVisible: false,
+    terrainProvider: terrainProvider, // 地形を表示
+    scene3DOnly: true, // 3D モードのパフォーマンスを最適化
 });
-viewer.scene.debugShowFramesPerSecond = true;
-// viewer.scene.skyBox.show = false;
-viewer.scene.sun.show = false;
-viewer.scene.moon.show = false;
-// viewer.scene.skyAtmosphere.show = false;
-// viewer.scene.fog.enabled = false;
-viewer.scene.globe.showWaterEffect = false;
-viewer.scene.globe.depthTestAgainstTerrain = true;
-viewer.scene.globe.backFaceCulling = false;
-// viewer.scene.globe.cartographicLimitRectangle = Cesium.Rectangle.fromDegrees(134.355368, 35.4591744, 134.3774097, 35.4727003);
 
 // 微地形表現図を表示
 viewer.scene.imageryLayers.addImageryProvider(
@@ -48,174 +22,220 @@ viewer.scene.imageryLayers.addImageryProvider(
     }),
 );
 
+// jsonファイルを取得する処理
 const getData = async (url) => {
-    const response = await window.fetch(url);
-    const featuresData = await response.json();
-    return featuresData;
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    return jsonData;
 };
 
-// 経緯度から楕円体高を求める
+// 経緯度から楕円体高を求める処理
 const getPositionsHeight = async (positions) => {
-    const terrainProvider = Cesium.createWorldTerrain();
     const promise = await Cesium.sampleTerrainMostDetailed(terrainProvider, positions);
     return promise;
 };
 
-// 変換処理
-const set3dData = async (url) => {
-    // geojsonファイルの取得
+// 立木のオブシェクトを生成する処理
+const createTree = async (url) => {
+    // jsonファイルの取得
     const jsonData = await getData(url);
 
     // ポイントの各座標から楕円体高をまとめて取得
     const positions = [];
-    jsonData.forEach((f) => {
-        // 経緯度から楕円体高を取得
-        positions.push(Cesium.Cartographic.fromDegrees(f.X, f.Y));
+    jsonData.forEach((data) => {
+        positions.push(Cesium.Cartographic.fromDegrees(data.X, data.Y));
     });
     const positionsEllipsoidal = await getPositionsHeight(positions);
 
-    // 立木の3Dオブジェクトの作成
+    // ジオメトリインスタンスの配列
     const treeInstance = [];
     const outlineInstance = [];
-    jsonData.forEach((f, i) => {
-        // if (i > 50) {
+
+    const labelCollection = new Cesium.LabelCollection();
+
+    // オブジェクト生成処理
+    jsonData.forEach((data, i) => {
+        // if (i > 10) {
         //     return;
         // }
-
-        // 樹高 (先端が出ないように少し低めに)
-        const treeHeight = f.H - f.Cl;
-        // 樹冠長
-        const treeCrownLength = f.Cl;
+        // 経度
+        const lng = data.X;
+        // 緯度
+        const lat = data.Y;
         // 枝下高
-        const trunkHeight = f.H - f.Cl;
-
-        // 楕円体高
-        const ellipsoidalHeight = positionsEllipsoidal[i].height;
+        const trunkHeight = data.H - data.Cl;
+        // 幹半径
+        const trunkRadius = data.D / 2;
+        // 樹冠長
+        const crownLength = data.Cl;
+        // 樹冠半径
+        const crownRadius = data.Bl;
         // 幹の高さの位置
-        const trunkPositionZ = ellipsoidalHeight + treeHeight / 2;
+        const trunkPositionZ = positionsEllipsoidal[i].height + trunkHeight / 2;
         // 樹冠の高さの位置
-        const crownPositionZ = ellipsoidalHeight + trunkHeight + treeCrownLength / 2;
+        const crownPositionZ = positionsEllipsoidal[i].height + trunkHeight + crownLength / 2;
 
-        switch (f.ID) {
+        // 樹種による色分け：スギ（１）、ヒノキ（２）、マツ（３）
+        switch (data.ID) {
             case 1:
-                trunkColor = [0, 100, 0, 250];
+                crownColor = [0, 100, 0];
                 break;
             case 2:
-                trunkColor = [0, 200, 40, 250];
+                crownColor = [0, 200, 40];
                 break;
             case 3:
-                trunkColor = [20, 200, 160, 250];
+                crownColor = [20, 200, 160];
                 break;
             default:
-                trunkColor = [0, 0, 0, 0];
+                crownColor = [0, 0, 0, 0];
         }
 
-        // 幹
-        const crownGeometry = new Cesium.CylinderGeometry({
-            length: f.H - f.Cl,
-            topRadius: f.D / 2,
-            bottomRadius: f.D / 2,
-            slices: 6,
-        });
-
-        const crownMatrix = Cesium.Matrix4.multiplyByTranslation(
-            Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(f.X, f.Y, trunkPositionZ)),
-            new Cesium.Cartesian3(0, 0, 0),
-            new Cesium.Matrix4(),
-        );
-
-        const crown3D = new Cesium.GeometryInstance({
-            geometry: crownGeometry,
-            modelMatrix: crownMatrix,
-            id: '幹 ' + String(i),
-            attributes: {
-                color: Cesium.ColorGeometryInstanceAttribute.fromColor(Cesium.Color.fromBytes(255, 154, 39, 250)),
-                show: new Cesium.ShowGeometryInstanceAttribute(true),
-            },
-        });
-        // 樹冠
-        const trunkGeometry = new Cesium.CylinderGeometry({
-            length: f.Cl,
-            topRadius: 0,
-            bottomRadius: f.Bl,
-            slices: 9,
-        });
-
+        // 幹のマトリックス
         const trunkMatrix = Cesium.Matrix4.multiplyByTranslation(
-            Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(f.X, f.Y, crownPositionZ)),
+            Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(lng, lat, trunkPositionZ)),
             new Cesium.Cartesian3(0, 0, 0),
             new Cesium.Matrix4(),
         );
 
-        const trunk3D = new Cesium.GeometryInstance({
-            geometry: trunkGeometry,
+        // 樹冠のマトリックス
+        const crownMatrix = Cesium.Matrix4.multiplyByTranslation(
+            Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(lng, lat, crownPositionZ)),
+            new Cesium.Cartesian3(0, 0, 0),
+            new Cesium.Matrix4(),
+        );
+
+        // 幹のジオメトリインスタンス：円柱
+        const trunkGeometryInstance = new Cesium.GeometryInstance({
+            geometry: new Cesium.CylinderGeometry({
+                length: trunkHeight,
+                topRadius: trunkRadius,
+                bottomRadius: trunkRadius,
+                slices: 6,
+            }),
             modelMatrix: trunkMatrix,
-            id: '樹冠 ' + String(i),
+            id: 'trunk' + String(i),
             attributes: {
-                color: Cesium.ColorGeometryInstanceAttribute.fromColor(Cesium.Color.fromBytes(...trunkColor)),
+                color: Cesium.ColorGeometryInstanceAttribute.fromColor(Cesium.Color.fromBytes(255, 154, 39, 200)),
                 show: new Cesium.ShowGeometryInstanceAttribute(true),
             },
         });
 
-        const outlineGeometry = new Cesium.CylinderOutlineGeometry({
-            length: f.Cl,
-            topRadius: 0,
-            bottomRadius: f.Bl,
-            slices: 9,
-        });
-
-        const outlineMatrix = Cesium.Matrix4.multiplyByTranslation(
-            Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(f.X, f.Y, crownPositionZ)),
-            new Cesium.Cartesian3(0, 0, 0),
-            new Cesium.Matrix4(),
-        );
-
-        const outline3D = new Cesium.GeometryInstance({
-            geometry: outlineGeometry,
-            modelMatrix: outlineMatrix,
-            id: 'outline ' + String(i),
+        // 樹冠のジオメトリインスタンス：円錐
+        const crownGeometryInstance = new Cesium.GeometryInstance({
+            geometry: new Cesium.CylinderGeometry({
+                length: crownLength,
+                topRadius: 0,
+                bottomRadius: crownRadius,
+                slices: 9,
+            }),
+            modelMatrix: crownMatrix,
+            id: 'crown' + String(i),
             attributes: {
-                color: Cesium.ColorGeometryInstanceAttribute.fromColor(Cesium.Color.fromBytes(255, 255, 255, 255)),
+                color: Cesium.ColorGeometryInstanceAttribute.fromColor(Cesium.Color.fromBytes(...crownColor, 200)),
                 show: new Cesium.ShowGeometryInstanceAttribute(true),
             },
         });
 
-        treeInstance.push(crown3D);
-        treeInstance.push(trunk3D);
-        outlineInstance.push(outline3D);
+        // 幹のアウトラインのジオメトリインスタンス
+        const trunkOutlineGeometryInstance = new Cesium.GeometryInstance({
+            geometry: new Cesium.CylinderOutlineGeometry({
+                length: trunkHeight,
+                topRadius: trunkRadius,
+                bottomRadius: trunkRadius,
+                slices: 6,
+            }),
+            modelMatrix: trunkMatrix,
+            id: 'trunkOutline' + String(i),
+            attributes: {
+                color: Cesium.ColorGeometryInstanceAttribute.fromColor(Cesium.Color.fromBytes(255, 154, 39, 255)),
+                show: new Cesium.ShowGeometryInstanceAttribute(true),
+            },
+        });
+
+        // 樹冠のアウトラインのジオメトリインスタンス
+        const crownOutlineGeometryInstance = new Cesium.GeometryInstance({
+            geometry: new Cesium.CylinderOutlineGeometry({
+                length: crownLength,
+                topRadius: 0,
+                bottomRadius: crownRadius,
+                slices: 9,
+            }),
+            modelMatrix: crownMatrix,
+            id: 'crownOutline' + String(i),
+            attributes: {
+                color: Cesium.ColorGeometryInstanceAttribute.fromColor(Cesium.Color.fromBytes(...crownColor, 255)),
+                show: new Cesium.ShowGeometryInstanceAttribute(true),
+            },
+        });
+
+        // 各インスタンスをそれぞれの配列に格納
+        treeInstance.push(trunkGeometryInstance);
+        treeInstance.push(crownGeometryInstance);
+        outlineInstance.push(trunkOutlineGeometryInstance);
+        outlineInstance.push(crownOutlineGeometryInstance);
     });
 
-    viewer.scene.primitives.add(
-        new Cesium.Primitive({
-            geometryInstances: treeInstance,
-            appearance: new Cesium.PerInstanceColorAppearance({
-                // flat: true,
-                // translucent: false,
-                faceForward: false,
-                // closed: true,
-            }),
-            vertexCacheOptimize: true,
-            compressVertices: true,
-            debugShowBoundingVolume: true,
-            interleave: true,
-        }),
-    );
+    // プリミティブの作成
+    const treePrimitive = new Cesium.Primitive({
+        geometryInstances: treeInstance,
+        appearance: new Cesium.PerInstanceColorAppearance(),
+    });
+    const outlinePrimitive = new Cesium.Primitive({
+        geometryInstances: outlineInstance,
+        appearance: new Cesium.PerInstanceColorAppearance(),
+        allowPicking: false,
+        releaseGeometryInstances: false,
+    });
 
-    viewer.scene.primitives.add(
-        new Cesium.Primitive({
-            geometryInstances: outlineInstance,
-            appearance: new Cesium.PerInstanceColorAppearance({
-                // translucent: false,
-                faceForward: false,
-            }),
-            // flat: false,
-            vertexCacheOptimize: true,
-            compressVertices: true,
-            interleave: true,
-            allowPicking: false,
-            appearance: new Cesium.PerInstanceColorAppearance({}),
-        }),
-    );
+    // プリミティブをシーンに追加
+    viewer.scene.primitives.add(treePrimitive);
+    viewer.scene.primitives.add(outlinePrimitive);
+
+    // シーンにラベルコレクションを追加
+    viewer.scene.primitives.add(labelCollection);
 };
 
-set3dData('treeTop.json');
+// 処理の実行
+createTree('treetop.json');
+
+// カメラの位置セット
+viewer.camera.setView({
+    destination: Cesium.Cartesian3.fromDegrees(134.358, 35.471, 900.0),
+    orientation: {
+        heading: Cesium.Math.toRadians(130.0),
+        pitch: Cesium.Math.toRadians(-35.0),
+    },
+});
+
+ // クリックイベント
+viewer.screenSpaceEventHandler.setInputAction(function (click) {
+
+    // 最も近いプリミティブを見つける
+    const pickedPrimitive = viewer.scene.pick(click.position);
+    if (Cesium.defined(pickedPrimitive) && pickedPrimitive.primitive instanceof Cesium.Primitive) {
+
+        // クリックしたジオメトリインスタンスのIDのナンバーを取得
+        const primitive = pickedPrimitive.primitive;
+        const attributes = primitive.getGeometryInstanceAttributes(pickedPrimitive.id);
+        if (Cesium.defined(attributes)) {
+            let targetID;
+            if (pickedPrimitive.id.includes('trunk')) {
+                targetID = pickedPrimitive.id.replace(/trunk/g, '');
+            } else if (pickedPrimitive.id.includes('crown')) {
+                targetID = pickedPrimitive.id.replace(/crown/g, '');
+            }
+
+            // 各ジオメトリインスタンスを取得
+            const trunk = viewer.scene.primitives._primitives[0].getGeometryInstanceAttributes('trunk' + targetID);
+            const crown = viewer.scene.primitives._primitives[0].getGeometryInstanceAttributes('crown' + targetID);
+            const trunkOutline = viewer.scene.primitives._primitives[1].getGeometryInstanceAttributes('trunkOutline' + targetID);
+            const crownOutline = viewer.scene.primitives._primitives[1].getGeometryInstanceAttributes('crownOutline' + targetID);
+
+            // 各ジオメトリインスタンスを非表示にする
+            trunk.show = Cesium.ShowGeometryInstanceAttribute.toValue(false);
+            crown.show = Cesium.ShowGeometryInstanceAttribute.toValue(false);
+            trunkOutline.show = Cesium.ShowGeometryInstanceAttribute.toValue(false);
+            crownOutline.show = Cesium.ShowGeometryInstanceAttribute.toValue(false);
+        }
+    }
+}, Cesium.ScreenSpaceEventType.LEFT_CLICK);
